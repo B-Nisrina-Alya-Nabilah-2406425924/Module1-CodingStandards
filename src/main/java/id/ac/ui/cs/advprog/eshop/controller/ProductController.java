19d5f0128bdb966e9c99e75fc11a4a98a0a927dc
Nisrina Alya Nabilah <nisrinaalyanabilah@gmail.com>
2026-02-07 10:32:48 +0700

    fixing minor bug

--- src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/controller/ProductController.java
@@ -22,7 +22,7 @@ public class ProductController {
         return "createProduct";
     }
 
-    @PatchMapping("/create")
+    @PostMapping("/create")
     public String createProductPost(@ModelAttribute Product product, Model model) {
         service.create(product);
         return "redirect:list";
