f440715f2bff7abe4d6658e8de85566af2a3d903
Nisrina Alya Nabilah <nisrina.alya@ui.ac.id>
2026-02-10 14:26:50 +0700

    adding create product functional test

---
+++ src/test/java/id/ac/ui/cs/advprog/eshop/functional/CreateProductFunctionalTest.java
@@ -0,0 +1,65 @@
+package id.ac.ui.cs.advprog.eshop.functional;
+
+import io.github.bonigarcia.seljup.SeleniumJupiter;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@ExtendWith(SeleniumJupiter.class)
+public class CreateProductFunctionalTest {
+
+    @LocalServerPort
+    private int serverPort;
+
+    @Value("${app.baseUrl:http://localhost}")
+    private String testBaseUrl;
+
+    private String baseUrl;
+
+    @BeforeEach
+    void setupTest() {
+        baseUrl = String.format("%s:%d", testBaseUrl, serverPort);
+    }
+
+    @Test
+    void createProduct_isSuccessful(ChromeDriver driver) throws Exception {
+        driver.get(baseUrl);
+        driver.findElement(By.linkText("Go to Product List")).click();
+        driver.findElement(By.linkText("Create Product")).click();
+
+        String productName = "Sampo Cap Bambang";
+        int productQuantity = 100;
+
+        WebElement nameInput = driver.findElement(By.id("nameInput"));
+        nameInput.clear();
+        nameInput.sendKeys(productName);
+
+        WebElement quantityInput = driver.findElement(By.id("quantityInput"));
+        quantityInput.clear();
+        quantityInput.sendKeys(String.valueOf(productQuantity));
+
+        driver.findElement(By.tagName("button")).click();
+
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(currentUrl.contains("/product/list"));
+
+        WebElement table = driver.findElement(By.tagName("table"));
+        String tableContent = table.getText();
+
+        assertTrue(tableContent.contains(productName));
+        assertTrue(tableContent.contains(String.valueOf(productQuantity)));
+    }
+}
