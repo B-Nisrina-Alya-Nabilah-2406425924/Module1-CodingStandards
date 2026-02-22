203471638b94120bb3e6e8c608696ed5685689a8
Nisrina Alya Nabilah <nisrina.alya@ui.ac.id>
2026-02-22 14:30:19 +0700

    fixing thymeleaf issues

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTests.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/ProductControllerTests.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
@@ -20,6 +21,9 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
+@TestPropertySource(properties = {
+        "spring.thymeleaf.enabled=false"
+})
 public class ProductControllerTests {
 
     @Autowired
