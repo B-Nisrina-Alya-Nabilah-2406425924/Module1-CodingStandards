5908efc35a0fc8a45c07f2eeda27ce7f327b21c0
Nisrina Alya Nabilah <nisrina.alya@ui.ac.id>
2026-03-03 11:33:32 +0700

    add README

--- src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
+++ src/test/java/id/ac/ui/cs/advprog/eshop/controller/CarControllerTest.java
@@ -1,16 +1,21 @@
 package id.ac.ui.cs.advprog.eshop.controller;
 
 import id.ac.ui.cs.advprog.eshop.service.CarService;
+import id.ac.ui.cs.advprog.eshop.service.CarService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CarController.class)
+@TestPropertySource(properties = {
+        "spring.thymeleaf.enabled=false"
+})
 public class CarControllerTest {
 
     @Autowired
